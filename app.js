be9d2c0ee15ae4ea50736505d81cf27879e4baa9
Peter <79867740+CodeMashine@users.noreply.github.com>
2023-03-18 22:17:03 +0300

    delete : delete some trash

--- app.js
+++ app.js
@@ -1,43 +1,20 @@
-"use strict" ;
+"use strict";
 
-const imageList = document.querySelectorAll(".item-image img") ;
-const modal = document.querySelector(".modal") ;
-const modelInner = document.querySelector(".modal-inner") ;
+const imageList = document.querySelectorAll(".item-image img");
+const modal = document.querySelector(".modal");
+const modelInner = document.querySelector(".modal-inner");
 
-function zoomHandler (event) {
-    // const img = event.target ;
-    // if (img.classList.contains("zoomIn")) {
-    //     img.classList.toggle("zoomIn") ;
-    // }else{
-    //     imageList.forEach( el => el.classList.remove("zoomIn")) ;
-    //     img.classList.add("zoomIn") ;
-    // }
-    // console.log("zoom") ;
-    // event.target.classList.toggle("zoomIn") ;
-    console.log(event.target) ;
-    showModal(event.target.src) ;
-} ;
-
-imageList.forEach( el => el.addEventListener("click" , (event)=>zoomHandler(event)))
-
-
-function showModal(img ){
-    console.log(modal.classList) ;
-    console.log(img) ;
-    modelInner.style.background = `center/contain no-repeat url(${img})`
-    // if(modal.classList.contains("active")){
-    //     modal.style.height = "100%" ;
-    //     modal.style.opacity = "0.6" ;
-    //     modal.style.zIndex = "999" ;
-    // }else{
-    //     modal.style.height = "0" ;
-    //     modal.style.opacity = "0" ;
-    //     modal.style.zIndex = "0" ;
-    // }
-    modal.classList.toggle("active") ;
+function zoomHandler(event) {
+  showModal(event.target.src);
 }
 
+imageList.forEach((el) =>
+  el.addEventListener("click", (event) => zoomHandler(event))
+);
 
+function showModal(img) {
+  modelInner.style.background = `center/contain no-repeat url(${img})`;
+  modal.classList.toggle("active");
+}
 
-
-modal.addEventListener("click" , showModal) ;
+modal.addEventListener("click", showModal);
